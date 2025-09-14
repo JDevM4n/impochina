cdf7696842b4bb4f230becf849d13dc8776579b3
Aslykmelo <aslycamelo857@gmail.com>
2025-09-14 13:28:37 -0500

    4

--- src/api/authService.js
+++ src/api/authService.js
@@ -1,14 +1,9 @@
-// src/api/authService.js
 import axios from "axios";
 
-const API_URL = "http://localhost:8001/auth";
-
-export const register = async (user) => {
-  const response = await axios.post(`${API_URL}/register`, user);
-  return response.data;
+export const register = (user) => {
+  return axios.post("http://localhost:8001/auth/register", user); // /auth si pusiste router.include con /auth
 };
 
-export const login = async (user) => {
-  const response = await axios.post(`${API_URL}/login`, user);
-  return response.data;
+export const login = (user) => {
+  return axios.post("http://localhost:8001/auth/login", user);
 };
