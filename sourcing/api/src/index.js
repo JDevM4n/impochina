79e734f46c82505e611144c215fd257b233ee4e6
Jesus Mantilla <97076292+JDevM4n@users.noreply.github.com>
2025-09-17 12:33:47 -0500

    Update index.js

--- sourcing/api/src/index.js
+++ sourcing/api/src/index.js
@@ -1,5 +1,6 @@
 // sourcing/api/src/index.js
 import express from "express";
+import cors from "cors";
 import { v4 as uuidv4 } from "uuid";
 import amqp from "amqplib";
 import {
@@ -13,6 +14,7 @@ import {
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 const RABBIT_URL = process.env.RABBIT_URL || "amqp://rabbitmq:5672";
